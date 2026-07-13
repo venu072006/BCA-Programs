aef4828f676ec61cea2e391a556725ae497ba69d
Venugopal K R <vg6850722@gmail.com>
2026-07-13 12:15:19 +0530

    13-07-26 R in-build function

---
+++ principal.R
@@ -0,0 +1,8 @@
+principal <-10000
+rate      <-8.5
+time      <-3
+simple_intrest <-(principal * rate * time)/100
+total_amount   <- principal + simple_intrest
+cat("principal     : Rs,",principal,  "\n")
+cat("simple_intrest: Rs,",simple_intrest,"\n")
+cat("total amount  : Rs,",total_amount,"\n")
